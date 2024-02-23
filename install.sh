8b2fd457a613371e365e60ad53346372f8c8be88
Plichso <Felix1221@live.de>
2024-02-23 02:01:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
